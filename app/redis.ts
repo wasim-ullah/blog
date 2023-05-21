9a6350e27796a320cfc55c3ef7cd52b2250ae856
Ze Sheng <zesh3ng@gmail.com>
2023-05-21 16:12:24 +1000

    update: redis token

--- app/redis.ts
+++ app/redis.ts
@@ -1,8 +1,8 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: "https://global-apt-bear-30602.upstash.io",
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: "https://usw1-mint-bulldog-33614.upstash.io" as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
 });
 
 export default redis;
