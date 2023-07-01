8f6400215a0ce710e3886c670ef6248f405c13f9
Wasim Ullah <wasim.ullah@aol.com>
2023-07-02 02:26:53 +0300

    Update header.tsx

--- app/(post)/header.tsx
+++ app/(post)/header.tsx
@@ -38,11 +38,11 @@ export function Header({ posts }: { posts: Post[] }) {
           <span className="hidden md:inline">
             <span>
               <a
-                href="https://twitter.com/"
+                href="https://linkedin.com/in/wasim-ullah"
                 className="hover:text-gray-800 dark:hover:text-gray-400"
                 target="_blank"
               >
-                @k20y
+                @wasim-ullah
               </a>
             </span>
 
