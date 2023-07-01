0837a551620a5ba2a3d256f1074411abc9cf9b43
Wasim Ullah <wasim.ullah@aol.com>
2023-07-02 02:21:10 +0300

    Update footer.tsx

--- app/footer.tsx
+++ app/footer.tsx
@@ -4,9 +4,9 @@ export function Footer() {
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
       <div className="grow text-left">
-        (
-        <A target="_blank" href="https://twitter.com/">
-          @wasimxullah
+        LinkedIn (
+        <A target="_blank" href="https://www.linkedin.com/in/wasim-ullah/">
+          wasim-ullah
         </A>
         )
       </div>
