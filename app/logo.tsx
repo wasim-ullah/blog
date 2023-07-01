30c3e16ac9b4eaa1b6b06af86c08453267af7547
Wasim Ullah <wasim.ullah@aol.com>
2023-07-02 02:03:06 +0300

    Update logo.tsx

--- app/logo.tsx
+++ app/logo.tsx
@@ -7,13 +7,13 @@ export function Logo() {
   return (
     <span className="text-md md:text-lg whitespace-nowrap font-bold">
       {pathname === "/" ? (
-        <span className="cursor-default pr-2">k20y</span>
+        <span className="cursor-default pr-2">Wasim Ullah</span>
       ) : (
         <Link
           href="/"
           className="hover:bg-gray-200 dark:hover:bg-[#313131] active:bg-gray-300 dark:active:bg-[#242424] p-2 rounded-sm -ml-2 transition-[background-color]"
         >
-          k20y
+          Wasim Ullah
         </Link>
       )}
     </span>
